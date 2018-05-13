a609639e792642e8ebbae5099d2b447dab032515
Noel Kuntze <noel@familie-kuntze.de>
2018-05-13 15:22:14 +0200

    Updated dmakepkg.py

--- bin/dmakepkg.py
+++ bin/dmakepkg.py
@@ -1,4 +1,4 @@
-#! /bin/python -B
+#! /bin/python3 -B
 
 import argparse
 import os
@@ -7,8 +7,25 @@ import sys
 import uuid
 
 class dmakepkg:
-	pacmanConf="/etc/pacman.conf"
-	makepkgConf="/etc/makepkg.conf"
+	def __init__(self):
+		self.pacmanConf="/etc/pacman.conf"
+		self.makepkgConf="/etc/makepkg.conf"
+		self.pacmanPkgCache="/var/cache/pacman/pkg"
+
+	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+	# Written by user Taejoon Byun
+	def getVar(self, script, varName):
+		CMD = 'echo $(source {}, echo ${})'.format(script, varName)
+		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+		return p.stdout.readlines()[0].decode("utf-8").strip()
+
+	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+	# Written by user Taejoon Byun
+	def callFunc(self, script, funcName):
+		CMD = 'echo $(source {}; echo $({}))'.format(script, funcName)
+		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+		return p.stdout.readlines()[0].decode("utf-8").strip()
+
 	def main(self):
 		self.parser = argparse.ArgumentParser(prog="dmakepkg")
 		self.parser.add_argument('-x',
@@ -28,26 +45,38 @@ class dmakepkg:
 
 		parameters = [ "--name", "dmakepkg_{}".format(uuid.uuid4())]
 
+		localCacheDir = self.getVar(self.pacmanConf, "CacheDir")
+		if not localCacheDir:
+			localCacheDir = "/var/cache/pacman/pkg"
+
 		if namespace.x:
-			parameters.extend("-v /etc/pacman.conf:/etc/pacman.conf".split(" "))
+			parameters.extend("-v /etc/pacman.conf:/etc/pacman.conf -v {}:{}".format(localCacheDir, localCacheDir).split(" "))
+		else:
+			parameters.extend("-v {}:{}".format(localCacheDir, self.pacmanPkgCache).split())
 
 		self.usePumpMode = namespace.y
 		self.downloadKeys = namespace.z
 		self.command = namespace.e
+		self.useHostPacman = namespace.x
 
-		parameters.extend(self.findParameters())
+		
+		if os.path.isfile(self.makepkgConf):
+			parameters += self.findParameters()
 		print("Parameters: ", parameters)
 
 		# set object attributes
 		# self.hostPacmanConf = namespace.
 		# create first part
-		completeCmdLine = "/bin/docker run --rm --net=host -ti".split(" ")
-		completeCmdLine += ["-v", "{}:/src".format(os.getcwd())] + parameters + [ "makepkg" ]
-		#completeCmdLine.extend(parameters)
+		completeCmdLine = "/bin/docker run --rm --net=host -ti --cpu-shares=128 --pids-limit=-1".split(" ")
 
-		if self.downloadKeys:
+		if self.useHostPacman:
+			completeCmdLine += [ "-v", "/etc/pacman.conf:/etc/pacman.conf" ]
+
+		completeCmdLine += ["-v", "{}:/src".format(os.getcwd())] + parameters + [ "makepkg" ]
+
+		if not self.downloadKeys:
 			completeCmdLine.append("-x")
-		if self.usePumpMode:
+		if not self.usePumpMode:
 			completeCmdLine.append("-y")
 		completeCmdLine.extend(["-u", str(os.geteuid()), "-g", str(os.getegid())])
 		if self.command:
@@ -67,13 +96,13 @@ class dmakepkg:
 			lines = open(self.makepkgConf, "r").readlines()
 		except:
 			# makepkg doesn't exist
-			pass
-		else:
-			parameters.extend("-v /etc/makepkg.conf:/etc/makepkg.conf".split())
-			for i in [ "SRCDEST", "PKGDEST", "SRCPKGDEST", "LOGDEST" ]:
-				if i in lines and not i.lstrip().startswith('#'):
-					tokens=i.lstrip().rstrip("\n").split("=")
-					parameters.append("{}:{}".format(tokens[1], tokens[1]))
+			return parameters
+	
+		parameters.extend("-v /etc/makepkg.conf:/etc/makepkg.conf".split())
+		for i in [ "SRCDEST", "PKGDEST", "SRCPKGDEST", "LOGDEST" ]:
+			if i in lines and not i.lstrip().startswith('#'):
+				tokens=i.lstrip().rstrip("\n").split("=")
+				parameters.append("{}:{}".format(tokens[1], tokens[1]))
 		return parameters
 
 if __name__ == '__main__':
