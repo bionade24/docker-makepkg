692bbcfaeda8c972059102eff7be3ed74126dbf8
bionade24 <o.roesler@oscloud.info>
2019-10-02 21:55:49 +0200

    Changed indentation for PEP 8 compatibility

--- bin/dmakepkg.py
+++ bin/dmakepkg.py
@@ -7,123 +7,123 @@ import sys
 import uuid
 
 class dmakepkg:
-	__eDefaults = "--nosign --force --syncdeps --noconfirm"
-	def __init__(self):
-		self.pacmanConf="/etc/pacman.conf"
-		self.makepkgConf="/etc/makepkg.conf"
-		self.pacmanPkgCache="/var/cache/pacman/pkg"
+    __eDefaults = "--nosign --force --syncdeps --noconfirm"
+    def __init__(self):
+        self.pacmanConf="/etc/pacman.conf"
+        self.makepkgConf="/etc/makepkg.conf"
+        self.pacmanPkgCache="/var/cache/pacman/pkg"
 
-	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
-	# Written by user Taejoon Byun
-	def getVar(self, script, varName):
-		CMD = 'echo $(source "{}"; echo ${{{}[@]}})'.format(script, varName)
-		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
-		return p.stdout.readlines()[0].decode("utf-8").strip()
+    # From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+    # Written by user Taejoon Byun
+    def getVar(self, script, varName):
+        CMD = 'echo $(source "{}"; echo ${{{}[@]}})'.format(script, varName)
+        p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        return p.stdout.readlines()[0].decode("utf-8").strip()
 
-	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
-	# Written by user Taejoon Byun
-	def callFunc(self, script, funcName):
-		CMD = 'echo $(source {}; echo $({}))'.format(script, funcName)
-		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
-		return p.stdout.readlines()[0].decode("utf-8").strip()
+    # From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+    # Written by user Taejoon Byun
+    def callFunc(self, script, funcName):
+        CMD = 'echo $(source {}; echo $({}))'.format(script, funcName)
+        p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        return p.stdout.readlines()[0].decode("utf-8").strip()
 
-	def signPackages(self):
-		args = [ "/bin/gpg", "--batch", "--yes", "--detach-sign" ]
-		key = self.getVar(self.makepkgConf, "GPGKEY")
-		if key:
-			args.extend(["-u", key])
-		f = []
-		for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
-			f.extend(filenames)
-			break
-		
-		for i in f:
-			if ".pkg." in i and not i.endswith("sig"):
-				g = []
-				g.extend(args)
-				g.append(i)
-				subprocess.run(g)
-
-
-
-	def main(self):
-		self.parser = argparse.ArgumentParser(prog="dmakepkg")
-		self.parser.add_argument('-x',
-			action='store_true',
-			help="Use host system's /etc/pacman.conf"
-			)
-		self.parser.add_argument('-y',
-			action='store_false',
-			help="Never use pump mode, even if pump mode capable servers are configured")
-		self.parser.add_argument('-z',
-			action='store_false',
-			help="Do not automatically download missing PGP keys",
-			)
-		self.parser.add_argument('-e', nargs='?',
-			help="Executes the argument as a command in the container after copying the package source")
-
-		self.parser.add_argument('rest', nargs=argparse.REMAINDER,
-			help="The arguments that are passed to the call to pacman in its executions in the container. They default to \"--nosign --force --syncdeps --noconfirm\".")
-
-		namespace = self.parser.parse_args()
-
-		parameters = [ "--name", "dmakepkg_{}".format(uuid.uuid4())]
-
-		# pacman.conf is not a bash file, so this doesn't work.
-		# localCacheDir = self.getVar(self.pacmanConf, "CacheDir")
-		# if not localCacheDir:
-		#	localCacheDir = "/var/cache/pacman/pkg"
-		localCacheDir = "/var/cache/pacman/pkg"
-
-		if namespace.x:
-			parameters.extend("-v /etc/pacman.conf:/etc/pacman.conf -v {}:{}".format(localCacheDir, localCacheDir).split(" "))
-		else:
-			parameters.extend("-v {}:{}".format(localCacheDir, self.pacmanPkgCache).split())
-
-		self.usePumpMode = namespace.y
-		self.downloadKeys = namespace.z
-		self.command = namespace.e
-		self.useHostPacman = namespace.x
-		
-		if os.path.isfile(self.makepkgConf):
-			parameters += self.findParameters()
-
-		# set object attributes
-		# self.hostPacmanConf = namespace.
-		# create first part
-		completeCmdLine = "/bin/docker run --rm -ti --cpu-shares=128 --pids-limit=-1".split(" ")
+    def signPackages(self):
+        args = [ "/bin/gpg", "--batch", "--yes", "--detach-sign" ]
+        key = self.getVar(self.makepkgConf, "GPGKEY")
+        if key:
+            args.extend(["-u", key])
+        f = []
+        for (dirpath, dirnames, filenames) in os.walk(os.getcwd()):
+            f.extend(filenames)
+            break
+        
+        for i in f:
+            if ".pkg." in i and not i.endswith("sig"):
+                g = []
+                g.extend(args)
+                g.append(i)
+                subprocess.run(g)
 
 
-		completeCmdLine += ["-v", "{}:/src".format(os.getcwd())] + parameters + [ "makepkg" ]
 
-		if not self.downloadKeys:
-			completeCmdLine.append("-x")
-		if not self.usePumpMode:
-			completeCmdLine.append("-y")
-		completeCmdLine.extend(["-u", str(os.geteuid()), "-g", str(os.getegid())])
-		if self.command:
-			completeCmdLine.extend(["-e", self.command])
-		completeCmdLine += namespace.rest
+    def main(self):
+        self.parser = argparse.ArgumentParser(prog="dmakepkg")
+        self.parser.add_argument('-x',
+            action='store_true',
+            help="Use host system's /etc/pacman.conf"
+            )
+        self.parser.add_argument('-y',
+            action='store_false',
+            help="Never use pump mode, even if pump mode capable servers are configured")
+        self.parser.add_argument('-z',
+            action='store_false',
+            help="Do not automatically download missing PGP keys",
+            )
+        self.parser.add_argument('-e', nargs='?',
+            help="Executes the argument as a command in the container after copying the package source")
 
-		dockerProcess = subprocess.Popen(completeCmdLine)
-		dockerProcess.wait()
+        self.parser.add_argument('rest', nargs=argparse.REMAINDER,
+            help="The arguments that are passed to the call to pacman in its executions in the container. They default to \"--nosign --force --syncdeps --noconfirm\".")
 
-		for i in self.getVar(self.makepkgConf, "BUILDENV").split():
-			if "sign" in i:
-				if not i.startswith("!"):
-					self.signPackages()
+        namespace = self.parser.parse_args()
 
-	# this function finds all possible arguments to the docker command line we could need
-	# and builds them.
-	def findParameters(self):
-		parameters=[ "-v", "/etc/makepkg.conf:/etc/makepkg.conf:ro" ]
+        parameters = [ "--name", "dmakepkg_{}".format(uuid.uuid4())]
 
-		for i in [ "SRCDEST", "PKGDEST", "SRCPKGDEST", "LOGDEST" ]:
-			value =  self.getVar(self.makepkgConf, i)
-			if value != "":
-				parameters.extend([ "-v",  "{}:{}".format(i, value)])
-		return parameters
+        # pacman.conf is not a bash file, so this doesn't work.
+        # localCacheDir = self.getVar(self.pacmanConf, "CacheDir")
+        # if not localCacheDir:
+        #    localCacheDir = "/var/cache/pacman/pkg"
+        localCacheDir = "/var/cache/pacman/pkg"
+
+        if namespace.x:
+            parameters.extend("-v /etc/pacman.conf:/etc/pacman.conf -v {}:{}".format(localCacheDir, localCacheDir).split(" "))
+        else:
+            parameters.extend("-v {}:{}".format(localCacheDir, self.pacmanPkgCache).split())
+
+        self.usePumpMode = namespace.y
+        self.downloadKeys = namespace.z
+        self.command = namespace.e
+        self.useHostPacman = namespace.x
+        
+        if os.path.isfile(self.makepkgConf):
+            parameters += self.findParameters()
+
+        # set object attributes
+        # self.hostPacmanConf = namespace.
+        # create first part
+        completeCmdLine = "/bin/docker run --rm -ti --cpu-shares=128 --pids-limit=-1".split(" ")
+
+
+        completeCmdLine += ["-v", "{}:/src".format(os.getcwd())] + parameters + [ "makepkg" ]
+
+        if not self.downloadKeys:
+            completeCmdLine.append("-x")
+        if not self.usePumpMode:
+            completeCmdLine.append("-y")
+        completeCmdLine.extend(["-u", str(os.geteuid()), "-g", str(os.getegid())])
+        if self.command:
+            completeCmdLine.extend(["-e", self.command])
+        completeCmdLine += namespace.rest
+
+        dockerProcess = subprocess.Popen(completeCmdLine)
+        dockerProcess.wait()
+
+        for i in self.getVar(self.makepkgConf, "BUILDENV").split():
+            if "sign" in i:
+                if not i.startswith("!"):
+                    self.signPackages()
+
+    # this function finds all possible arguments to the docker command line we could need
+    # and builds them.
+    def findParameters(self):
+        parameters=[ "-v", "/etc/makepkg.conf:/etc/makepkg.conf:ro" ]
+
+        for i in [ "SRCDEST", "PKGDEST", "SRCPKGDEST", "LOGDEST" ]:
+            value =  self.getVar(self.makepkgConf, i)
+            if value != "":
+                parameters.extend([ "-v",  "{}:{}".format(i, value)])
+        return parameters
 
 if __name__ == '__main__':
-	dm = dmakepkg()
-	dm.main()
+    dm = dmakepkg()
+    dm.main()
--- run.py
+++ run.py
@@ -11,180 +11,180 @@ import shlex
 import sys
 
 def eprint(*args, **kwargs):
-	print(*args, file=sys.stderr, **kwargs)
+    print(*args, file=sys.stderr, **kwargs)
 
 class dmakepkgContainer:
-	__restDefaults = "--nosign --force --syncdeps --noconfirm"
+    __restDefaults = "--nosign --force --syncdeps --noconfirm"
 
-	# From https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth/12514470
-	# Written by user atzz
-	def copytree(self, src, dst, symlinks=False, ignore=None):
-		for item in os.listdir(src):
-			s = os.path.join(src, item)
-			d = os.path.join(dst, item)
-			if os.path.isdir(s):
-				shutil.copytree(s, d, symlinks, ignore)
-			else:
-				shutil.copy2(s, d)
+    # From https://stackoverflow.com/questions/1868714/how-do-i-copy-an-entire-directory-of-files-into-an-existing-directory-using-pyth/12514470
+    # Written by user atzz
+    def copytree(self, src, dst, symlinks=False, ignore=None):
+        for item in os.listdir(src):
+            s = os.path.join(src, item)
+            d = os.path.join(dst, item)
+            if os.path.isdir(s):
+                shutil.copytree(s, d, symlinks, ignore)
+            else:
+                shutil.copy2(s, d)
 
-	# to not change either gid or uid, set that value to -1.
-	# From https://stackoverflow.com/questions/2853723/what-is-the-python-way-for-recursively-setting-file-permissions
-	# Written by user "too much php"
-	def changeUserOrGid(self, uid, gid, path):
-		for root, dirs, files in os.walk(path):
-			for momo in dirs:
-				os.chown(os.path.join(root, momo), uid, gid)
-			for momo in files:
-				os.chown(os.path.join(root, momo), uid, gid)
+    # to not change either gid or uid, set that value to -1.
+    # From https://stackoverflow.com/questions/2853723/what-is-the-python-way-for-recursively-setting-file-permissions
+    # Written by user "too much php"
+    def changeUserOrGid(self, uid, gid, path):
+        for root, dirs, files in os.walk(path):
+            for momo in dirs:
+                os.chown(os.path.join(root, momo), uid, gid)
+            for momo in files:
+                os.chown(os.path.join(root, momo), uid, gid)
 
-	# From https://www.tutorialspoint.com/How-to-change-the-permission-of-a-directory-using-Python
-	# Written by Rajendra Dharmkar
-	def changePermissionsRecursively(self, path, mode):
-		for root, dirs, files in os.walk(path, topdown=False):
-			for dir in [os.path.join(root,d) for d in dirs]:
-				os.chmod(dir, mode)
-			for file in [os.path.join(root, f) for f in files]:
-				os.chmod(file, mode)
+    # From https://www.tutorialspoint.com/How-to-change-the-permission-of-a-directory-using-Python
+    # Written by Rajendra Dharmkar
+    def changePermissionsRecursively(self, path, mode):
+        for root, dirs, files in os.walk(path, topdown=False):
+            for dir in [os.path.join(root,d) for d in dirs]:
+                os.chmod(dir, mode)
+            for file in [os.path.join(root, f) for f in files]:
+                os.chmod(file, mode)
 
-	def appendToFile(self, path, content):
-		with open(path, "a+") as f:
-			f.seek(0,2)
-			f.write(content)
+    def appendToFile(self, path, content):
+        with open(path, "a+") as f:
+            f.seek(0,2)
+            f.write(content)
 
-	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
-	# Written by user Taejoon Byun
-	def getVar(self, script, varName):
-		CMD = 'echo $(source "{}"; echo ${{{}[@]}})'.format(script, varName)
-		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
-		return p.stdout.readlines()[0].decode("utf-8").strip()
+    # From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+    # Written by user Taejoon Byun
+    def getVar(self, script, varName):
+        CMD = 'echo $(source "{}"; echo ${{{}[@]}})'.format(script, varName)
+        p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        return p.stdout.readlines()[0].decode("utf-8").strip()
 
-	# From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
-	# Written by user Taejoon Byun
-	def callFunc(self, script, funcName):
-		CMD = 'echo $(source "{}"; echo $({}))'.format(script, funcName)
-		p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
-		return p.stdout.readlines()[0].decode("utf-8").strip()
+    # From https://stackoverflow.com/questions/17435056/read-bash-variables-into-a-python-script
+    # Written by user Taejoon Byun
+    def callFunc(self, script, funcName):
+        CMD = 'echo $(source "{}"; echo $({}))'.format(script, funcName)
+        p = subprocess.Popen(CMD, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        return p.stdout.readlines()[0].decode("utf-8").strip()
 
-	def checkForPumpMode(self):
-		distccHosts = self.getVar("/etc/makepkg.conf", "DISTCC_HOSTS")
-		if ",cpp" in distccHosts and self.usePumpMode:
-			return True
-		return False
+    def checkForPumpMode(self):
+        distccHosts = self.getVar("/etc/makepkg.conf", "DISTCC_HOSTS")
+        if ",cpp" in distccHosts and self.usePumpMode:
+            return True
+        return False
 
-	def main(self):
-		self.parser = argparse.ArgumentParser(prog="dmakepkgContainer")
-		self.parser.add_argument('-e',
-			nargs='?',
-			help="CMD to run the command after the source directory was copied")
-		self.parser.add_argument('-g',
-			nargs='?',
-			help="The GID to own any created package (Ignored unless UID is also provided")
-		self.parser.add_argument('-p',
-			action='store_true',
-			help="Run a pacman -Syu before building")
-		self.parser.add_argument('-u',
-			nargs='?',
-			help="UID to own any created package")
-		self.parser.add_argument('-y',
-			action='store_false',
-			help="Do not use pump mode.")
-		self.parser.add_argument('-z',
-			action='store_false',
-			help="Do not automatically download missing PGP keys")
+    def main(self):
+        self.parser = argparse.ArgumentParser(prog="dmakepkgContainer")
+        self.parser.add_argument('-e',
+            nargs='?',
+            help="CMD to run the command after the source directory was copied")
+        self.parser.add_argument('-g',
+            nargs='?',
+            help="The GID to own any created package (Ignored unless UID is also provided")
+        self.parser.add_argument('-p',
+            action='store_true',
+            help="Run a pacman -Syu before building")
+        self.parser.add_argument('-u',
+            nargs='?',
+            help="UID to own any created package")
+        self.parser.add_argument('-y',
+            action='store_false',
+            help="Do not use pump mode.")
+        self.parser.add_argument('-z',
+            action='store_false',
+            help="Do not automatically download missing PGP keys")
 
-		namespace, self.rest = self.parser.parse_known_args()
+        namespace, self.rest = self.parser.parse_known_args()
 
-		if not os.path.isfile("/src/PKGBUILD") or os.path.islink("/src/PKGBUILD"):
-			eprint("No PKGBUILD file found! Aborting.")
-			sys.exit(1)
+        if not os.path.isfile("/src/PKGBUILD") or os.path.islink("/src/PKGBUILD"):
+            eprint("No PKGBUILD file found! Aborting.")
+            sys.exit(1)
 
-		self.command = namespace.e
-		self.group = int(namespace.g)
-		self.runPacmanSyu = namespace.p
-		self.user = int(namespace.u)
-		self.usePumpMode = namespace.y
-		self.downloadKeys = namespace.z
-		buildUserUid = pwd.getpwnam("build-user").pw_uid
-		buildUserGid = pwd.getpwnam("build-user").pw_gid
-		self.copytree("/src/", "/build")
-		self.changeUserOrGid(buildUserUid, buildUserGid, "/build")
+        self.command = namespace.e
+        self.group = int(namespace.g)
+        self.runPacmanSyu = namespace.p
+        self.user = int(namespace.u)
+        self.usePumpMode = namespace.y
+        self.downloadKeys = namespace.z
+        buildUserUid = pwd.getpwnam("build-user").pw_uid
+        buildUserGid = pwd.getpwnam("build-user").pw_gid
+        self.copytree("/src/", "/build")
+        self.changeUserOrGid(buildUserUid, buildUserGid, "/build")
 
-		
+        
 
-		if self.runPacmanSyu:
-			arguments = "pacman --noconfirm -Syu".split()
-			pacmanProcess = subprocess.Popen(arguments)
-			pacmanProcess.wait()
-		else:
-			arguments = "pacman --noconfirm -Sy".split()
-			pacmanProcess = subprocess.Popen(arguments)
-			pacmanProcess.wait()
-		flags = None
+        if self.runPacmanSyu:
+            arguments = "pacman --noconfirm -Syu".split()
+            pacmanProcess = subprocess.Popen(arguments)
+            pacmanProcess.wait()
+        else:
+            arguments = "pacman --noconfirm -Sy".split()
+            pacmanProcess = subprocess.Popen(arguments)
+            pacmanProcess.wait()
+        flags = None
 
-		if len(self.rest) == 0:
-			flags = self.__restDefaults.split()
-		else:
-			# translate list object to space seperated arguments
-			flags = self.rest
+        if len(self.rest) == 0:
+            flags = self.__restDefaults.split()
+        else:
+            # translate list object to space seperated arguments
+            flags = self.rest
 
-		if self.downloadKeys:
-			gnupg = os.path.expanduser("~build-user/.gnupg")
-			os.makedirs(gnupg, mode=0o700, exist_ok=True)
-			self.changeUserOrGid(buildUserUid, pwd.getpwnam("build-user").pw_gid, "/build")
-			self.changePermissionsRecursively(gnupg, 0o700)
-			self.appendToFile(gnupg + "/gpg.conf", "keyserver-options auto-key-retrieve\n")
-			self.changePermissionsRecursively(gnupg + "/gpg.conf", 0o600)
+        if self.downloadKeys:
+            gnupg = os.path.expanduser("~build-user/.gnupg")
+            os.makedirs(gnupg, mode=0o700, exist_ok=True)
+            self.changeUserOrGid(buildUserUid, pwd.getpwnam("build-user").pw_gid, "/build")
+            self.changePermissionsRecursively(gnupg, 0o700)
+            self.appendToFile(gnupg + "/gpg.conf", "keyserver-options auto-key-retrieve\n")
+            self.changePermissionsRecursively(gnupg + "/gpg.conf", 0o600)
 
-		# if a command is specified in -e, then run it
-		if self.command:
-			args = shlex.split(self.command)
-			subprocess.run(args)
+        # if a command is specified in -e, then run it
+        if self.command:
+            args = shlex.split(self.command)
+            subprocess.run(args)
 
-		# su resets PATH, so distcc doesn't find the distcc directory
-		if self.checkForPumpMode():
-			bashFileContents="#! /bin/bash\n"
-			"pump makepkg {}\n".format(" ".join(flags))
-			with open("/buildScript.sh", "w") as f:
-				f.write(bashFileContents)
-			self.changePermissionsRecursively("/buildScript.sh", 0o555)
-			arguments = [ 'su', '-c' ] +  [ 'DISTCC_HOSTS="{}" DISTCC_LOCATION={} pump makepkg {}'.format(self.getVar("/etc/makepkg.conf", "DISTCC_HOSTS"),
-				"/usr/bin",
-				" ".join(flags)) ] + [ '-s', '/bin/bash', 'build-user' ]
-			makepkgProcess = subprocess.run(arguments)
+        # su resets PATH, so distcc doesn't find the distcc directory
+        if self.checkForPumpMode():
+            bashFileContents="#! /bin/bash\n"
+            "pump makepkg {}\n".format(" ".join(flags))
+            with open("/buildScript.sh", "w") as f:
+                f.write(bashFileContents)
+            self.changePermissionsRecursively("/buildScript.sh", 0o555)
+            arguments = [ 'su', '-c' ] +  [ 'DISTCC_HOSTS="{}" DISTCC_LOCATION={} pump makepkg {}'.format(self.getVar("/etc/makepkg.conf", "DISTCC_HOSTS"),
+                "/usr/bin",
+                " ".join(flags)) ] + [ '-s', '/bin/bash', 'build-user' ]
+            makepkgProcess = subprocess.run(arguments)
 
-			# while makepkgProcess.poll() == None:
-			# 	outs, errs = makepkgProcess.communicate(input="")
-			# 	if outs:
-			# 		print(outs)
-			# 	if errs:
-			# 		eprint(errs)
-		else:
-			arguments = [ 'su', '-c'] +  [ 'makepkg {}'.format(" ".join(flags)) ] + [ '-s', '/bin/bash', '-l', 'build-user']
-			makepkgProcess = subprocess.Popen(arguments)
-			while makepkgProcess.poll() == None:
-				outs, errs = makepkgProcess.communicate(input="")
-				if outs:
-					print(outs)
-				if errs:
-					eprint(errs)
+            # while makepkgProcess.poll() == None:
+            #     outs, errs = makepkgProcess.communicate(input="")
+            #     if outs:
+            #         print(outs)
+            #     if errs:
+            #         eprint(errs)
+        else:
+            arguments = [ 'su', '-c'] +  [ 'makepkg {}'.format(" ".join(flags)) ] + [ '-s', '/bin/bash', '-l', 'build-user']
+            makepkgProcess = subprocess.Popen(arguments)
+            while makepkgProcess.poll() == None:
+                outs, errs = makepkgProcess.communicate(input="")
+                if outs:
+                    print(outs)
+                if errs:
+                    eprint(errs)
 
-		if self.user and not self.group:
-			self.changeUserOrGid(self.user, self.group, "/build")
-		elif self.user:
-			self.changeUserOrGid(self.user, -1, "/build")
+        if self.user and not self.group:
+            self.changeUserOrGid(self.user, self.group, "/build")
+        elif self.user:
+            self.changeUserOrGid(self.user, -1, "/build")
 
-		# copy any packages
-		# use globbing to get all packages
-		for i in glob.iglob("/build/*pkg.tar*"):
-			shutil.copy(i, "/src")
-		else:
-			if not len(flags):
-				eprint("No packages were built!")
-				sys.exit(2)
-		sys.exit(0)
+        # copy any packages
+        # use globbing to get all packages
+        for i in glob.iglob("/build/*pkg.tar*"):
+            shutil.copy(i, "/src")
+        else:
+            if not len(flags):
+                eprint("No packages were built!")
+                sys.exit(2)
+        sys.exit(0)
 
 if __name__ == "__main__":
-	containerEntrypoint = dmakepkgContainer()
-	containerEntrypoint.main()
+    containerEntrypoint = dmakepkgContainer()
+    containerEntrypoint.main()
 
 #"PATH" : self.getVar("~build-user/.bashrc", "PATH")
